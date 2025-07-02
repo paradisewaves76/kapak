07357102a1a55f702cb55a823daa14faecba67ae
dreamtechdev230 <essekherif205@gmail.com>
2025-07-02 18:52:21 +0300

    Delete remove.py

--- remove.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def remove_package(name):
-    path = f"/usr/local/bin/{name}"
-    if os.path.exists(path):
-        os.remove(path)
-        print(f"[+] {name} kaldırıldı.")
-    else:
-        print(f"[!] {path} bulunamadı.")
